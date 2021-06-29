089dd827e7c4af1f19600207591bc69aaec30d00
sanzhi <19250584@life.hkbu.edu.hk>
2021-06-29 22:53:54 +0800

    initially commit

--- main.py
+++ main.py
@@ -84,12 +84,15 @@ s = full_data3.investment.isin(
     ['Direct investment assets', 'Net international investment position of direct   inventment',
      'Direct investment liabilities'])
 
+
 full_data3['investment'] = s
 for b in range(1, len(full_data3)):
-    cc = full_data3.iloc[[b - 1], [3]].values.item(0)
-
+    cc = full_data3.iloc[[b-1],[3]]
+    # if cc == True:
     print(type(cc))
-    print(cc)
+
+
+
 
 # full_data3['investment'].loc(full_data3['investment']).isin(
 #     [' Direct investment assets', ' Net international investment position of direct   inventment',
